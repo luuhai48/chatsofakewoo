5bda7b1370f26e7ba21c08a482ddbb1178530c48
Hai Luu <hai.luu@advesa.com>
2022-07-10 09:54:38 +0700

    refactor

--- deps.ts
+++ deps.ts
@@ -1,112 +1,89 @@
 import { Router, Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 
-export default (opts = {}) => {
+const generateHTML = (opts = {}) => {
   const defaultOpts = {
     body: "<h1>Hello World</h1>",
     header: "",
-    footer: ""
-  }
-  const {header, footer, body} = { ...defaultOpts, ...opts };
-  const html = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chatso Store</title>
-    ${header}
-  </head>
-  <body>
-    ${body}
-    ${footer}
-  </body>
-</html>`;
+    footer: "",
+    title: "Hello World",
+  };
+  const { header, footer, body, title } = { ...defaultOpts, ...opts };
 
-  const authHtml = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Woocommerce Auth</title>
-  <style>
-    button {
-      background-color: #4CAF50;
-      color: white;
-      padding: 14px 30px;
-      margin: 8px 0;
-      border: none;
-      cursor: pointer;
-      border-radius: 5px;
-      font-weight: bold;
-    }
-    button.disabled {
-      pointer-events: none;
-    }
-  </style>
-</head>
-<body>
-  <div class="container">
-    <button>Authenticate</button>
-    <div class="message"></div>
-  </div>
-  <script>
-    const rand = (size) =>
-      [...Array(size)]
-        .map(() => Math.floor(Math.random() * 16).toString(16))
-        .join("");
-    const createKeys = (appName, userId, scope) => {
-      return {
-        user_id: userId,
-        key_permissions: scope,
-        consumer_key: 'ck_' + rand(40),
-        consumer_secret: 'cs_' + rand(40),
-      };
+  return `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>${title}</title>
+      ${header}
+    </head>
+    <body>
+      ${body}
+      ${footer}
+    </body>
+  </html>`;
+};
+
+export default (opts = {}) => {
+  const defaultHtml = generateHTML(opts);
+  const authHtml = generateHTML({
+    title: "WooCommerce Authenticate",
+    header: `<style>button {background-color: #4CAF50;color: white;padding: 14px 30px;margin: 8px 0;border: none;cursor: pointer;border-radius: 5px;font-weight: bold;}button.disabled {pointer-events: none;}.container {display: flex;justify-content: center;align-items: center;}</style>`,
+    body: `<div class="container">
+  <button>Authenticate</button>
+  <div class="message"></div>
+</div>`,
+    footer: `<script>
+  const rand = (size) => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join("");
+  const createKeys = (appName, userId, scope) => {
+    return {
+      user_id: userId,
+      key_permissions: scope,
+      consumer_key: 'ck_' + rand(40),
+      consumer_secret: 'cs_' + rand(40),
     };
-    const btn = document.querySelector('button');
-    btn.onclick = () => {
-      const message = document.querySelector('.message');
-      message.innerHTML = 'Authenticating...';
-      btn.classList.add('disabled');
-      const searchParams = new URLSearchParams(window.location.search);
-      const appName = searchParams.get('app_name');
-      const userId = searchParams.get('user_id');
-      const scope = searchParams.get('scope');
-      const returnUrl = searchParams.get('return_url');
-      const callbackUrl = searchParams.get('callback_url');
-      fetch(callbackUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-        },
-        body: JSON.stringify(createKeys(appName, userId, scope))
-      }).then(res => {
-        if (res.ok) {
-          message.innerHTML = 'Authenticated. Redirecting...';
-          setTimeout(() => {
-            window.location.href = returnUrl;
-          }, 2000);
-        } else {
-          message.innerHTML = 'Authentication failed';
-          btn.classList.remove('disabled');
-        }
-      }).catch(err => {
-        message.innerHTML = 'Authentication failed';
-        btn.classList.remove('disabled');
-      });
-    };
-  </script>
-</body>
-</html>`
+  };
+  const btn = document.querySelector('button');
+  const message = document.querySelector('.message');
+  const showError = () => {
+    message.innerHTML = '<span style="color: red; font-weight: bold;">Authentication failed</span>';
+    btn.classList.remove('disabled');
+  }
+  btn.onclick = () => {
+    message.innerHTML = 'Authenticating...';
+    btn.classList.add('disabled');
+    const params = new URLSearchParams(window.location.search);
+    fetch(params.get('callback_url'), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(createKeys(params.get('app_name'), params.get('user_id'), params.get('scope')))
+    }).then(res => {
+      if (res.ok) {
+        message.innerHTML = 'Authenticated. Redirecting...';
+        setTimeout(() => {
+          window.location.href = params.get('return_url');
+        }, 2000);
+      } else {
+        showError();
+      }
+    }).catch(() => {
+      showError();
+    });
+  };
+</script>`,
+  });
 
   const router = new Router();
   router.get("/wc-auth/v1/authorize", (context) => {
     context.response.body = authHtml;
   });
   router.get("/", (context) => {
-    context.response.body = html;
+    context.response.body = defaultHtml;
   });
-  router.get("/index.html", (context) => {
-    context.response.body = html;
+  router.get("/:anythingelse", (context) => {
+    context.response.body = defaultHtml;
   });
 
   const app = new Application();
--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
-import App from './deps.ts';
+import App from "./deps.ts";
 
 const footer = '<div class="footer">&copy; 2020</div>';
-const app = App({footer});
+const app = App({ title: "How are your?", footer });
 app.listen({ port: 8000 });
